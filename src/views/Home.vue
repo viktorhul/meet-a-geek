94268aad821d8b4e7ac2e66e4ec82635e3e5d23b
Viktor Hultsten <viktor.hultsten@gmail.com>
2022-01-31 13:21:00 +0100

    Tidy up a little

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="https://www.icegif.com/wp-content/uploads/icegif-4790.gif"
+    />
+    <HelloWorld msg="Meet-a-Geek.com" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
