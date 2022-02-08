7a2d8dc4bf3418c8ec14262f3ff858129f1e7951
Viktor Hultsten <viktor.hultsten@gmail.com>
2022-02-08 13:44:20 +0100

    Added some styling to fill viewport

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,19 @@
 </template>
 
 <style>
+html,
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
 }
 
 #nav {
