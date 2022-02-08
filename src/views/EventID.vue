b9a521a230292fd8b70878fb7afc7e800f8b9fd2
Jonas Bjork <jonas.betula@gmail.com>
2022-02-08 13:26:58 +0100

    EventID init

---
+++ src/views/EventID.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="EventId">
+    <h1>Lets Match</h1>
+    <h3>Event ID</h3>
+    <input v-model="id" placeholder="_ _ _ _">
+    <button>OK</button>
+    
+  </div>
+
+
+</template>
