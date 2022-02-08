d7168109d3ee98de6ef27b6ae2c502e033fde40f
ExOster <71136452+ExOster@users.noreply.github.com>
2022-02-08 21:45:57 +0100

    geek review base

---
+++ src/components/randomprofile.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="card">
+    <div class="image-cropper">
+    <img src="https://placebeard.it/640x360" alt="John" style="width:100%" >
+    </div>
+    <h2>Viking Dude</h2>
+    <p class="title">Some cool anekdote or statement</p>
+    <h4>Location:</h4>
+    <p>Woods of sweeden</p>
+    <h3>Some other information...</h3>
+    <p>Pretends to be a GamerGirls sometimes...</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+
+button:hover, a:hover {
+  opacity: 0.7;
+}
+
+
+</style>
---
+++ src/views/GeekReview.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="geekreview">
+  <h1>Geek Review</h1>
+  <div>
+   <randomprofile></randomprofile>
+  </div>
+  <br>
+    <h2>Meet again? 1</h2>
+    <div>
+      <standard_button></standard_button>
+      <standard_button></standard_button>
+    </div>
+    <br>
+    <h5>Question 1</h5>
+    <div>
+      <standard_button></standard_button>
+    </div>
+    <br>
+    <h5>Question 2</h5>
+    <div>
+      <standard_button></standard_button>
+    </div>
+    <br>
+    <h5>Question 3</h5>
+    <div>
+      <standard_button></standard_button>
+    </div>
+    <br>
+    <h5>Question 4</h5>
+    <div>
+      <standard_button></standard_button>
+    </div>
+    <br>
+    <h5>Question 5</h5>
+    <div>
+      <standard_button></standard_button>
+    </div>
+    <br>
+  </div>
+
+</template>
+
+<script>
+import standard_button from "@/components/standard_button";
+import randomprofile from "@/components/randomprofile";
+export default {
+  name: "GeekReview",
+  components: {standard_button,randomprofile}
+}
+</script>
+
+<style scoped>
+
+</style>
