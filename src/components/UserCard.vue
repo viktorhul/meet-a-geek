6da29ef4e829f9e89543cf803deeb8a57fdfca33
AlbinBergkvist <46403697+AlbinBergkvist@users.noreply.github.com>
2022-02-08 14:46:43 +0100

    endScreen layout

---
+++ src/components/UserCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+    <img class="profilePic" src="../assets/logo.png" alt="profilbild">
+    <h3 class="userName">Albin</h3>
+    </div>
+</template>
+
+<style>
+.profilePic{
+    width: 20%;
+    float: left;
+
+}
+.container{
+    border-style: solid;
+    border-color: red;
+    width: 20%;
+    height: 30%;
+    border-radius: 10%;
+}
+.userName{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+</style>
