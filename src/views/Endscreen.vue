1b6067c824b9907cb0430991eeeae2c9f3c769c0
AlbinBergkvist <46403697+AlbinBergkvist@users.noreply.github.com>
2022-02-08 14:46:43 +0100

    endScreen layout

---
+++ src/views/Endscreen.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="EventOverTitle">
+    <h1>Event Over!</h1>
+    <h2>Matches:</h2>
+    <UserCard />
+    <UserCard />
+    <UserCard />
+  </div>
+</template>
+
+<script>
+import UserCard from "@/components/UserCard.vue";
+
+export default{
+    components:{
+        UserCard,
+    }
+    
+}
+</script>
