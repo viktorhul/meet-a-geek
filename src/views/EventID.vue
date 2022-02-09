f973f915f518f1c557f5ace59529b29a6fd2d10d
Jonas Bjork <jonas.betula@gmail.com>
2022-02-09 09:37:55 +0100

    some updates

--- src/views/EventID.vue
+++ src/views/EventID.vue
@@ -3,9 +3,18 @@
     <h1>Lets Match</h1>
     <h3>Event ID</h3>
     <input v-model="id" placeholder="_ _ _ _">
+    <br>
     <button>OK</button>
-    
   </div>
 
 
 </template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+
+</style>
