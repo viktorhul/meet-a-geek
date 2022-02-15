312a52f37a547abbf844dda01c1bdc49a94369d2
Jonas Bjork <jonas.betula@gmail.com>
2022-02-15 10:31:28 +0100

    step 2

--- src/views/EventID.vue
+++ src/views/EventID.vue
@@ -2,19 +2,23 @@
   <div class="EventId">
     <h1>Lets Match</h1>
     <h3>Event ID</h3>
-     <!-- <input type="text" id="username" name="username" maxlength="10"><br><br> -->
-    <input v-model="id" placeholder="_ _ _ _" maxlength="4" minlength="4"/>
+    <!-- <input type="text" id="username" name="username" maxlength="10"><br><br> -->
+    <input v-model="id" placeholder="_ _ _ _" maxlength="4" minlength="4" />
     <br /><br />
-    <button @click="goToNext()" disabled=true>OK</button>
+    <standard_button @click="clickAction">Login</standard_button>
   </div>
 </template>
 
 <script>
+import standard_button from "@/components/standard_button";
 export default {
+  components:{
+    standard_button
+  },
   methods: {
-    goToNext() {
+    clickAction() {
       //Explain pls
-      this.$router.push('/about');
+      this.$router.push('/endscreen');
     },
   },
 };
