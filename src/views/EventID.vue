cf1d48017097023345393d16ccfa0ee4e9e489f7
Jonas Bjork <jonas.betula@gmail.com>
2022-02-15 10:02:57 +0100

    init update

--- src/views/EventID.vue
+++ src/views/EventID.vue
@@ -2,23 +2,27 @@
   <div class="EventId">
     <h1>Lets Match</h1>
     <h3>Event ID</h3>
-    <input v-model="id" placeholder="_ _ _ _">
-    <br><br>
-    <button>OK</button>
+     <!-- <input type="text" id="username" name="username" maxlength="10"><br><br> -->
+    <input v-model="id" placeholder="_ _ _ _" maxlength="4" minlength="4"/>
+    <br /><br />
+    <button @click="goToNext()" disabled=true>OK</button>
   </div>
-
-
 </template>
 
 <script>
-
+export default {
+  methods: {
+    goToNext() {
+      //Explain pls
+      this.$router.push('/about');
+    },
+  },
+};
 </script>
 
-
 <style scoped>
-input{
+input {
   text-align: center;
   width: 4em;
 }
-
 </style>
