8af3f294cb6a93f9ba94852b599fd03fe427ccb8
AlbinBergkvist <46403697+AlbinBergkvist@users.noreply.github.com>
2022-02-15 09:41:17 +0100

    Datescreen layout

---
+++ src/components/MoveToTable.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>MOVE TO</h2>
+        <div class="tableContainer">
+            <h3>TABLE 8</h3>
+        </div>
+        <h4>WAITING FOR HOST</h4>
+    </div>
+</template>
+<style scoped>
+.tableContainer{
+    border-style: solid;
+    width: 30%;
+}
+</style>
---
+++ src/views/Datescreen.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <header class="DateHeader"> Date 1</header>
+    <h1>MATCH FOUND!</h1>
+    <UserProfileMatch />
+    <MoveToTable />
+</div>
+</template>
+
+<script>
+import UserProfileMatch from "@/components/UserProfileMatch.vue";
+import MoveToTable from "@/components/MoveToTable.vue";
+export default{
+    components:{
+        UserProfileMatch,
+        MoveToTable,
+    }
+    
+}
+</script>
+
+<style scoped>
+.DateHeader{
+    
+}
+</style>
