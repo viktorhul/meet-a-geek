7c9000d29861aa07ed97e616174c27727cd41b7b
AlbinBergkvist <46403697+AlbinBergkvist@users.noreply.github.com>
2022-02-21 13:54:42 +0100

    functionality without backend

---
+++ src/components/CountdownTimer.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <p id="timeleft" v-if="timeleft > 0">{{timeleft}}</p>
+    <p style="font-weight:bold;" v-else>Times up</p>
+</div>
+</template>
+
+<script>
+
+
+export default{
+    data() {
+        return {
+            timeleft: 10,
+            timer: null,
+
+        }
+    },
+    created(){
+
+        this.timer= setInterval(() =>{
+            this.countdown()
+
+        },1000)
+    },
+     methods:{
+         countdown(){
+             this.timeleft --;
+            if(this.timeleft == 0){
+                clearInterval(this.timer);
+            }
+         }
+     } 
+}
+</script>
