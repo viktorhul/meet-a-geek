bdea179e99e27e99aceddcece01785b2e66f6983
Jonas Bjork <jonas.betula@gmail.com>
2022-03-01 08:29:38 +0100

    Id box

--- src/views/EventID.vue
+++ src/views/EventID.vue
@@ -2,14 +2,15 @@
   <div class="EventId">
     <h1>Lets Match</h1>
     <h3>Event ID</h3>
-    <!-- <input type="text" id="username" name="username" maxlength="10"><br><br> -->
     <input
+      id="inputthing"
       @keyup="checkLength"
       v-model="id"
       placeholder="_ _ _ _"
       maxlength="4"
       minlength="4"
     />
+    <div v-if="wrongPassword"><p>HEJEHJE</p></div>
     <br /><br />
     <standard_button @click="clickAction" :disable="!validInputData"
       >Login</standard_button
@@ -25,15 +26,27 @@ export default {
   },
   methods: {
     clickAction() {
-      this.$router.push("/endscreen");
+      if (this.checkIfValidId(this.id)) {
+        this.$router.push("/endscreen");
+      }
+this.wrongPassword = true;
+      setTimeout(() => {
+        this.id = "";true
+      }, 2000);
+      console.log("TITTA HIT");
+      this.wrongPassword = false;
+      // this.id=""
     },
     checkLength() {
       console.log(this.id.length);
       return (this.validInputData = this.id.length > 3);
     },
+    checkIfValidId(id) {
+      return id === "LOVE";
+    },
   },
   data() {
-    return { validInputData: false, id: "" };
+    return { validInputData: false, wrongPassword: false, id: "" };
   },
 };
 </script>
