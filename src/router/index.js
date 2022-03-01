ab26fd34ee17eea4642481e53923775c456d9552
Viktor Hultsten <viktor.hultsten@gmail.com>
2022-03-01 16:44:09 +0100

    Added routing to create-account

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Endscreen from '../views/Endscreen.vue'
 import Datescreen from '../views/Datescreen.vue'
 import Admin from '../views/Admin.vue'
 import Loading from '../views/Loading.vue'
+import CreateAccount from '@/views/CreateAccount.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -23,14 +24,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
- 
+
     path: '/GeekReview',
     name: 'GeekReview',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/GeekReview.vue')
-},
+  },
   {
     path: '/eventid',
     name: 'EventID',
@@ -55,6 +56,11 @@ const routes = [
     path: '/loading',
     name: 'Loading',
     component: Loading
+  },
+  {
+    path: '/create-account',
+    name: 'CreateAccount',
+    component: CreateAccount
   }
 ]
 
