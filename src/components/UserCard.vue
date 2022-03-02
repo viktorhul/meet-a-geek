bc61c230fd4136118ed03b69b28e710ab345c623
Viktor Hultsten <viktor.hultsten@gmail.com>
2022-03-02 20:53:12 +0100

    added props and another format: small and big

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,41 +1,121 @@
 <template>
-  <div class="card">
-      <br>
+  <div class="card card-big" v-if="format == 'big'">
+    <br />
     <div class="image-cropper">
-      <img src="https://placebeard.it/640x360" alt="profile pic" class="avatar" >
+      <img :src="picture" alt="profile pic" class="avatar avatar-big" />
     </div>
-    <h2>Viking Dude</h2>
-    <p class="title">Some cool anekdote or statement</p>
+    <h2>{{ fullname }}</h2>
+    <p class="title">{{ statement }}</p>
     <h4>Location:</h4>
-    <p>Woods of sweden</p>
-    <h3>Some other information...</h3>
-    <p>Pretends to be a GamerGirls sometimes...</p>
+    <p>{{ location }}</p>
+    <h3>{{ other }}</h3>
+    <p>{{ secret }}</p>
+  </div>
+
+  <div v-else-if="format == 'small'" class="card card-small">
+    <div class="profileContainer">
+      <img :src="picture" class="avatar avatar-small" />
+    </div>
+    <div class="textbox">
+      <h3>{{ fullname }}</h3>
+      <p>{{ location }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-}
+  props: {
+    picture: {
+      type: String,
+      default: "https://placebeard.it/640x360",
+    },
+    fullname: {
+      type: String,
+      default: "Viking Dude",
+    },
+    statement: {
+      type: String,
+      default: "Some cool anekdote or statement",
+    },
+    location: {
+      type: String,
+      default: "Woods of sweden",
+    },
+    other: {
+      type: String,
+      default: "Some other information...",
+    },
+    secret: {
+      type: String,
+      default: "Pretends to be a GamerGirls sometimes...",
+    },
+    format: {
+      type: String,
+      default: "big",
+    },
+  },
+};
 </script>
 
 <style scoped>
 .card {
-    /* THIS NEED TO CHANGE */
-  background-color: #ACB992;
+  /* THIS NEED TO CHANGE */
+  background-color: #acb992;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
+  width: 300px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.card-big {
   border-radius: 10%;
   margin: auto;
   text-align: center;
 }
-.avatar {
 
+.card-small {
+  display: flex;
+  flex-direction: row;
+  border-radius: 5px;
+}
+
+.card-small .textbox {
+  text-align: left;
+  height: 100%;
+  margin-left: 10px;
+}
+
+.textbox h3 {
+  margin: 0;
+  font-size: 16pt;
+  font-weight: bold;
+}
+
+.textbox p {
+  margin: 0;
+}
+
+.profileContainer {
+  height: 100%;
+  align-content: center;
+}
+
+.avatar {
   vertical-align: middle;
+  border-radius: 50%;
+}
+
+.avatar-big {
   width: 150px;
   height: 150px;
-  border-radius: 50%;
   border: 7px solid lightgray;
+}
+
+.avatar-small {
+  width: 50px;
+  height: 50px;
+  border: 2px solid lightgray;
 }
 
 .title {
@@ -62,9 +142,8 @@ a {
   color: black;
 }
 
-button:hover, a:hover {
+button:hover,
+a:hover {
   opacity: 0.7;
 }
-
-
 </style>
